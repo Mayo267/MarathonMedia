5680b360dc5b9ad3cf521140b8002247e02bb5cd
Andrew <amayo0925@gmail.com>
2022-04-02 16:12:25 -0400

    Added BrowserRouter and Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './Pages/Home'
+import News from './Pages/News'
+import Reviews from './Pages/Reviews'
+import Contact from './Pages/Contact'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
+import Profile from './Pages/Profile';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <Router>
+     {/* Navbar here */}
+      <Link to="/"> Home </Link>
+      <Link to="/News"> News </Link>
+      <Link to="/Reviews"> Reviews </Link>
+      <Link to="/Contact"> Contact </Link>
+      <Link to="/Login"> Login </Link>
+      <Link to="/Signup"> Signup </Link>
+      <Link to="/Profile"> Profile </Link>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/News" element={<News/>} />
+        <Route path="/Reviews" element={<Reviews/>} />
+        <Route path="/Contact" element={<Contact/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Signup" element={<Signup/>} />
+        <Route path="/Profile" element={<Profile/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/Template.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Template Page for all pages with different content inside
+function Template() {
+    return(
+        <div>
+            Template Page
+        </div>
+    )
+}
+
+export default Template
